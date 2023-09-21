3b43523a0e323f6808a88cff502bc58730bdab91
ivarcodes <114813138+ivarcodes@users.noreply.github.com>
2023-09-21 09:53:16 +0530

    Update Day3.cpp

--- Day3.cpp
+++ Day3.cpp
@@ -1,8 +1,18 @@
 #include<iostream>;
 using namespace std;
 int main(){
-    cout<<"hello world"<<endl;
-    cout<<2+3;
+    cout<<"hello world"<<endl;//print statement
+    cout<<2+3;//addition of two numbers
+//variables are container used to store the values 
+//datatype are the type of data to be stored in it
+int num1=10;//int datatype , num1 is variable name
+String name="Ravi "; //string datatype,name is variable name
+decimal num2=1245.9;//decimal is datatype ,num2 is variable name
+float num3=2.0//float is similar to the decimal but it stores smaller values
+bool true=0;//true or false
+
+    
+    
     
     
 }
