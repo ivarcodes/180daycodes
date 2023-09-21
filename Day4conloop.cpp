e388e2d75452fa32de2a673ee3a64fc3c07409cb
ivarcodes <114813138+ivarcodes@users.noreply.github.com>
2023-09-21 09:47:23 +0530

    Add files via upload
    conditional statements in c++ and looping

---
+++ Day4conloop.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //Even or odd 
+    
+    //cin>> is used to get input from user
+    // int num;
+    // cout<<"enter the number:";
+    // cin>>num;
+    // if(num%2==0){//condition check
+    //     cout<<"Even number";
+    // }
+    // else{
+    //     cout<<"odd number";
+    // }
+    
+    //a> b
+    
+    // int a ;
+    // int b;
+    // cout<<"Enter number 1:";
+    // cin>>a;
+    // cout<<"Enter number 2:";
+    // cin>>b;
+    // if(a>b){
+    //     cout<<"a is greater than b";
+    // }
+    // else{
+    //     cout<<"b is greater than a";
+    // }
+    
+    
+    //homework
+    
+    // if Age is greater than 18 print adult or print teenager
+    
+    // int age; 
+    // cout<<"enter your age";
+    // cin>>age;
+    // if(age>18){
+    //     cout<<"adult";
+        
+        
+    // }
+    // else{
+    //     cout<<"teenager";
+    // }
+    
+    //check number is +ve,-ve or 0
+    
+    // int number;
+    // cout<<"enter the Number:";
+    // cin>>number;
+    // if(number>0){
+    //     cout<<"Number is positive";
+    // }
+    // else if(number==0){
+    //     cout<<"Number is Zero";
+    // }
+    // else{
+    //     cout<<"Number is negative";
+    // }
+    
+    //vowel a,e,i,o,u else consonant
+    
+    // char c;
+    // cout<<"enter character";
+    // cin>>c;
+    // if(c=='a'||c=='e'||c=='e'||c=='i'||c=='o'||c=='u'){
+    //     cout<<"Vowel";
+        
+    // }
+    // else{
+    //     cout<<"consonant";
+    // }
+    //print number 1 to 10
+    for(int i=1;i<=5;i++){
+        cout<<i<<endl;
+    }
+    //print a name 10 times
+    for(int i=0;i<10;i++){
+        cout<<"Ravi"<<endl;
+    }
+    
+    //sqaure of the Number
+    for(int i=1;i<4;i++){
+        cout<<i*i<<endl;
+    }
+}
