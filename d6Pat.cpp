cfc24bbd8f65972a061bc7fef9e3bb7dda7a537a
ivarcodes <114813138+ivarcodes@users.noreply.github.com>
2023-09-25 07:59:09 +0530

    Add files via upload
    Day 6: pattern questions using for loop in c++

---
+++ d6Pat.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // // Day6: pattern printing using for loop in c++
+    // for(int j=1;j<=5;j++){
+    //      for(int i=1;i<=5;i++){
+    //     cout<<"*"  <<" ";
+    // }
+    // cout<<endl;
+    // }
+//     int num=10;
+//   for(int i=1;i<=4;i++){ //row (runs 4 times)
+//       for(int j=1;j<=5;j++){//col(prints stars 5 times )
+//           cout<<num<<" ";
+//       }
+//       cout<<endl;
+//   }
+// for(int i=1;i<=5;i++){
+//     for(int j=1;j<=5;j++){
+//         cout<< i <<" "; //printing row 5 times
+//     }
+//     cout<<endl; //new line 
+    
+// }
+
+// for(int i=1;i<=5;i++){
+//     for(int j=1;j<=5;j++){
+//         cout<<j<<" ";
+//     }
+//     cout<<endl;
+// }
+
+// for(int i=1;i<=5;i++){
+//     for(int j=5;j>=1;j-- ){
+//         cout<<j<<" ";
+//     }
+//     cout<<endl;
+// }
+// //sqrt question( home work )
+// for(int i=1;i<=5;i++){
+//     for(int j=1;j<=5;j++){
+//         cout<<j*j<<" ";
+//     }
+//     cout<<endl;
+// }
+
+//cube( home work )
+
+// for(int i=1;i<=5;i++){
+//     for(int j=1;j<=5;j++){
+//         cout<<j*j*j<<" ";
+//     }
+//     cout<<endl;
+// }
+
+// for(char i='a';i<='e';i++){
+//     for(int j=1;j<=5;j++){
+//         cout<<i<<" ";
+//     }
+//     cout<<endl;
+// }
+// }
+
+//alphabets ( home work )
+// for(int i=1;i<=5;i++){
+//     for(char j='a';j<='e';j++){
+//         cout<<j<<" ";
+//     }
+//     cout<<endl;
+// }
+// }
+int count=1;
+for(int i=1;i<=5;i++){
+    for(int j=1;j<=5;j++){
+        cout<<count<<" ";
+        count++;
+    }
+    cout<<endl;
+}
+}
+   
+    
