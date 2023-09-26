ca349a1a6634baa6766d3ca2dd859e95db6a7b08
ivarcodes <114813138+ivarcodes@users.noreply.github.com>
2023-09-26 10:23:06 +0530

    Add files via upload
    Day 7/180
    pattern problem solving using for loops in c++

---
+++ D7pat.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // for(int i=5;i>=1;i--){
+    //     for(int j=1;j<=i;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    // for(int i=5;i>=1;i--){
+    //     for(int j=1;j<=i;j++){
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    
+    // home work 1st question
+//  for(int i=1;i<=5;i++){
+//      for(int j=1;j<=i;j++){
+//          cout<<j<<" ";
+//      }
+//      cout<<endl;
+//  }
+        //2nd question
+        
+//   for (int i = 1; i <= 5; i++) {
+//         for (char j = 'a'; j <= 'a' + i - 1; j++) {
+//             cout << j << ' ';
+//         }
+        
+//         cout << endl;
+//     }
+    //3rd question
+    
+    // for(int i=1;i<=6;i++){
+    //   int count=10;
+    //     for(int j=1;j<=i;j++,count++){
+    //         cout<<count<<" ";
+    //     }
+    //      cout<<endl;
+    //     }
+    
+    //4th question
+    
+    
+    // for(int i=5;i>=1;i--){
+    //     char k='a';
+    //     for(int j=1;j<=i;j++,k++){
+    //         cout<<k<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    //completed day 7 :)
+      
+    }
+        
+    
+
+
+
+  
+        
+    
+
