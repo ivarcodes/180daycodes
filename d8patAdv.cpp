bc994bd5a7ae3493f86ff9f8bacd15718cc31c99
ivarcodes <114813138+ivarcodes@users.noreply.github.com>
2023-09-27 11:47:12 +0530

    Add files via upload
    Day 8/180 
    advanced pattern in c++ using  loops

---
+++ d8patAdv.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5;
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i;j++){
+    //         cout<<"  ";
+    //     }
+    //     for(int j=1;j<=i;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    
+    // }
+    
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i;j++){
+    //         cout<<"  ";
+    //     }
+    //     for(int j=1;j<=i;j++){
+    //         cout<<i<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i;j++){
+    //         cout<<"   ";
+    //     }
+    //     for(int j=1;j<=i;j++){
+    //         cout<<j<<"  ";
+            
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=1;i<=n;i++){
+        
+    //     for(int j=1;j<=n-i;j++){
+    //         cout<<"  ";
+    //     }
+    //     char let='A';
+    //     for(int j=1;j<=i;j++,let++){
+    //         cout<<let<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+     for(int i=1;i<=5;i++){
+         for(int j=1;j<=n-i;j++){
+             cout<<"   ";
+         }
+         for(int j=i;j>=1;j--){
+             cout<<j<<"  ";
+         }
+         cout<<endl;
+     }
+    
+    
+}
+     
